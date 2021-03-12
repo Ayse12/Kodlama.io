b55ab1e41b5ecbf46dc056d9b866e9f5048a9958
AYŞE İLHANLI <20162013027@std.nisantasi.edu.tr>
2021-03-12 12:20:23 +0300

    Linq

---
+++ CSharp/LinqProject/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category> { 
+                new Category{CategoryId=1,CategoryName="Bilgisayar"},
+                new Category{CategoryId=2,CategoryName="Telefon"},
+            };
+            List<Product> products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1,ProductName="Acer Laptop",QuantityPerUnit="32 GB RAM",UnitPrice=5000,UnitsInStock=3 },
+                new Product{ProductId=2, CategoryId=1,ProductName="ASUS Laptop",QuantityPerUnit="8 GB RAM",UnitPrice=1000,UnitsInStock=5 },
+                new Product{ProductId=3, CategoryId=1,ProductName="HP Laptop",QuantityPerUnit="16 GB RAM",UnitPrice=1000,UnitsInStock=2 },
+                new Product{ProductId=4, CategoryId=2,ProductName="Samsung ",QuantityPerUnit="64 GB RAM",UnitPrice=6000,UnitsInStock=10 },
+                new Product{ProductId=5, CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit="16 GB RAM",UnitPrice=7000,UnitsInStock=7 },
+                
+            };
+            Console.WriteLine("Algoritmik-----------------");
+            foreach (var product in products)
+            {
+                if (product.UnitPrice>5000 && product.UnitsInStock>3)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+                
+            }
+            Console.WriteLine("Linq-----------");
+            var result = products.Where(p => p.UnitPrice > 4000 && p.UnitsInStock>3);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+        
+
+    }
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
